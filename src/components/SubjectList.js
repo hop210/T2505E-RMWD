96a3af25102c42c29ba9f7109597434baf998514
hop210 <hopcbvn5@gmail.com>
2025-09-18 18:45:12 +0700

    update subject

--- src/components/SubjectList.js
+++ src/components/SubjectList.js
@@ -14,13 +14,7 @@ function SubjectList() {
       <h2 className="text-2xl font-bold mb-2 uppercase">
         What is the course program?
       </h2>
-      <p className="max-w-3xl mx-auto mb-8 text-gray-600">
-        With a program design that includes 06 semesters with many important
-        technologies such as ReactJS, PHP, Python, .NET Core, Spring Boot,
-        students are trained to become confident programmers. After graduation,
-        students are confident in front of employers with 4 great semesters with
-        quality, practical products, students will definitely score points.
-      </p>
+      
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-8 justify-items-center">
         {subjects.map((subj) => (
@@ -40,6 +34,13 @@ function SubjectList() {
 
         ))}
       </div>
+      <p className="max-w-3xl mx-auto mb-8 text-gray-600">
+        With a program design that includes 06 semesters with many important
+        technologies such as ReactJS, PHP, Python, .NET Core, Spring Boot,
+        students are trained to become confident programmers. After graduation,
+        students are confident in front of employers with 4 great semesters with
+        quality, practical products, students will definitely score points.
+      </p>
     </section>
   );
 
